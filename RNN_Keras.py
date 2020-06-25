a577ac0a58527e3284bf0238ed246dd8e7eb573f
Vedaant Tambi <tambi@wisc.edu>
2020-06-25 19:26:23 -0400

    Simplified RNN Keras. Prepared the dataset for the model.

--- RNN_Keras.py
+++ RNN_Keras.py
@@ -26,7 +26,7 @@ import gc
 
 def choose_validation_set_indices(file_name, index):
     """
-    Chooses the validation_set according to the process number. The
+    Chooses the test_set according to the process number. The
     :param file_name:
     :param index: The model is learnt on a different part of the data set every time
     :return:
@@ -34,16 +34,21 @@ def choose_validation_set_indices(file_name, index):
     pickle_in = open(file_name, 'rb')
     nfold_arr = pickle.load(pickle_in)  # For the purpose of this project, n=5
     pickle_in.close()
-    # nfold_arr[index] stores another list
+    # nfold_arr[index] stores another list which contains 20% of the total no. of indices of validation set (8 surfaces)
     validation_set = nfold_arr[index]
     return validation_set
 
 
 def load_pickle_array(folder, file):
-    pickle_in = open(folder + file, "rb")
-    arr = pickle.load(pickle_in)
+    """
+    This loads or converts the pickled file into the training set and validation set data
+    :param folder: the folder that contains the training/validaiton data
+    :param file:
+    :return:
+    """
+    pickle_in = open(folder + file, "rb")  # '+' performs the join operation
+    arr = np.array(pickle.load(pickle_in))  # converts the list to a numpy array
     pickle_in.close()
-    arr = np.array(arr)
     print(file, 'loaded')
     return arr
 
@@ -55,112 +60,77 @@ if __name__ == "__main__":
     nDeep = sys.argv[3]
     validate_input_folder = train_input_folder = sys.argv[4]
     output_folder = ""
-    i, j, k = index, index, index
-    norm_method = sys.argv[5]
-    img_rows = int(sys.argv[6])
+    norm_method = sys.argv[5]  # referring to the choice of normalizing frame-wise or all-wise
+    img_rows = int(sys.argv[6])  # the number of rows in the image
     img_cols = int(sys.argv[7])
-    # file_name = int(sys.argv[8])
     batch_size = 128  # Batch size
     nb_epoch = 50  # Number of times data is passed through network
-    n_split = -1  # Ensures reading of arrays as 1 contiguous array file rather than n_split different pickles
 
     start_time = time.time()
 
+    # Loading the train and test set
+    X_train = load_pickle_array(train_input_folder, 'X_train_np.pickle').astype('float16')
+    X_validate = load_pickle_array(validate_input_folder, 'X_validate_np.pickle').astype('float16')
+    y_train = load_pickle_array(train_input_folder, 'y_train.pickle').astype('float32')
+    y_validate = load_pickle_array(validate_input_folder, 'y_validate.pickle').astype('float32')
 
-    print(train_input_folder)
-
-    if (n_split == -1):
-        X_train_curr = load_pickle_array(train_input_folder, 'X_train_np.pickle')
-        X_validate_curr = load_pickle_array(validate_input_folder, 'X_validate_np.pickle')
-        y_train_curr = load_pickle_array(train_input_folder, 'y_train.pickle')
-        y_validate_curr = load_pickle_array(validate_input_folder, 'y_validate.pickle')
-    elif (n_split > 0):
-        for i in range(n_split):
-            train_x_str = 'X_train_np_' + str(i)
-            val_x_str = 'X_validate_np_' + str(i)
-            train_y_str = 'y_train_' + str(i)
-            val_y_str = 'y_validate_' + str(i)
-            if (i == 0):
-                X_train_curr = load_pickle_array(train_input_folder, train_x_str)
-                X_validate_curr = load_pickle_array(validate_input_folder, val_x_str)
-                y_train_curr = load_pickle_array(train_input_folder, train_y_str)
-                y_validate_curr = load_pickle_array(validate_input_folder, val_y_str)
-            else:
-                X_train_curr_temp = load_pickle_array(train_input_folder, train_x_str)
-                X_validate_curr_temp = load_pickle_array(validate_input_folder, val_x_str)
-                y_train_curr_temp = load_pickle_array(train_input_folder, train_y_str)
-                y_validate_curr_temp = load_pickle_array(validate_input_folder, val_y_str)
-                X_train_curr = np.concatenate((X_train_curr, X_train_curr_temp))
-                X_validate_curr = np.concatenate((X_validate_curr, X_validate_curr_temp))
-                y_train_curr = np.concatenate((y_train_curr, y_train_curr_temp))
-                y_validate_curr = np.concatenate((y_validate_curr, y_validate_curr_temp))
-    X_train_curr = X_train_curr.astype('float16')
-    X_validate_curr = X_validate_curr.astype('float16')
-    print('Converted X arrays to float16')
-
-    ## Converting y arrays to float16 does not lead to substantial size reduction, and kills all values ##
-    y_train_curr = y_train_curr.astype('float32')
-    y_validate_curr = y_validate_curr.astype('float32')
-
-    # ~~~~~~~~~~ Normalize with max value of entire density array ~~~~~~~~~~ #
+    # Normalize with max value of entire density array
     if (norm_method == 'all'):
-        X_train_curr /= np.max(X_train_curr)
-        X_validate_curr /= np.max(X_validate_curr)
+        X_train /= np.max(X_train)
+        X_validate /= np.max(X_validate)
         print('Arrays normalized with max value')
 
-    ## ~~~~~~~~~~ Normalize with max value of each frame ~~~~~~~~~~ #
+    # Normalize with max value of each frame
     if (norm_method == 'frame'):
-        train_max_1 = np.zeros((X_train_curr.shape[0], 1))
-        for count_1 in range(nDeep):
-            train_max_0 = np.array([np.max(x) for x in X_train_curr[:, :, :, count_1]])
-            train_max_0 = np.reshape(train_max_0, (train_max_0.shape[0], 1))
-            train_max_1 = np.hstack((train_max_1, train_max_0))
-        train_max_1 = np.delete(train_max_1, 0, axis=1)
+        train_max = np.zeros((X_train.shape[0], 1))
+        for frame in range(nDeep):
+            next = np.array([np.max(x) for x in X_train[:, :, :, frame]])
+            next = np.reshape(next, (next.shape[0], 1))
+            train_max = np.hstack((train_max, next))
+        train_max = np.delete(train_max, 0, axis=1)
 
-        val_max_1 = np.zeros((X_validate_curr.shape[0], 1))
-        for count_2 in range(nDeep):
-            val_max_0 = np.array([np.max(x) for x in X_validate_curr[:, :, :, count_2]])
-            val_max_0 = np.reshape(val_max_0, (val_max_0.shape[0], 1))
-            val_max_1 = np.hstack((val_max_1, val_max_0))
-        val_max_1 = np.delete(val_max_1, 0, axis=1)
+        val_max = np.zeros((X_validate.shape[0], 1))
+        for frame in range(nDeep):
+            next = np.array([np.max(x) for x in X_validate[:, :, :, frame]])
+            next = np.reshape(next, (next.shape[0], 1))
+            val_max = np.hstack((val_max, next))
+        val_max = np.delete(val_max, 0, axis=1)
 
-        X_train_curr /= train_max_1[:, np.newaxis, np.newaxis, :] # np.new
-        X_validate_curr /= val_max_1[:, np.newaxis, np.newaxis, :]
+        X_train /= train_max[:, np.newaxis, np.newaxis, :]  # np.new
+        X_validate /= val_max[:, np.newaxis, np.newaxis, :]
         print('Arrays normalized with frame-wise max value')
 
-    y_unique = np.unique(y_validate_curr)
+    # To keep only validation set entries in validation array, and to delete these from the training set
+    y_unique = np.unique(y_validate)
     validate_entries = y_unique[choose_validation_set_indices(file_name, index)]
     for val in validate_entries:
-        X_train_curr = np.delete(X_train_curr, np.where(y_train_curr == val), axis=0)
-        y_train_curr = np.delete(y_train_curr, np.where(y_train_curr == val), axis=0)
-    # Keep only validation set entries in validation array
-    shape = np.shape(X_train_curr)
+        # np.where returns the array of indices where the values in the training set coincide with the values in the
+        # validation set. np.delete removes these values (from X and y)
+        X_train = np.delete(X_train, np.where(y_train == val), axis=0)
+        y_train = np.delete(y_train, np.where(y_train == val), axis=0)
 
-    if (nDeep == 1):
-        X_train_curr = X_train_curr[:, :, :, 0]
-
-    X_train_curr = X_train_curr.reshape((shape[0], shape[1], shape[2], nDeep))
+    unflattened_shape = X_train.shape
+    print("Shape of X_train:", unflattened_shape)
+    X_train = X_train.reshape((unflattened_shape[0], -1, nDeep))
 
     # Definition of size of problem
     print('Defining network')
-    nb_classes = len(np.unique(y_train_curr))  # Number of classes
+    nb_classes = len(np.unique(y_train))  # Number of classes
 
-    y_train_1 = (y_train_curr - np.mean(y_train_curr)) / np.std(y_train_curr)
-    y_validate_1 = (y_validate_curr - np.mean(y_train_curr)) / np.std(y_train_curr)
-    y_train_1 = (y_train_curr - np.mean(y_train_curr)) / np.std(y_train_curr)
+    # Standardization of the labels of training and validation set
+    y_validate = (y_validate - np.mean(y_train)) / np.std(y_train)
+    y_train = (y_train - np.mean(y_train)) / np.std(y_train)
 
-    print('X_train shape:', X_train_curr.shape)
-    print('Y_train shape:', len(y_train_curr))
-    print('X_test shape:', X_validate_curr.shape)
+    print('X_train shape:', X_train.shape)
+    print('y_train shape:', y_train.shape)
+    print('X_test shape:', X_validate.shape)
+    print('y_test shape:', y_validate)
 
     # Shape of input - 32x32x1
-    in_shape = (img_rows, img_cols, nDeep)
+    in_shape = ( nDeep)
 
     model = Sequential()
 
-    # model.add(Convolution2D(2, (5, 5), activation = 'relu', kernel_initializer='he_normal', input_shape=in_shape, name='conv1'))
-
-    # model.add(Convolution2D(12, (5, 5), activation = 'relu', kernel_initializer='he_normal', input_shape=in_shape, name='conv1'))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -187,25 +157,18 @@ if __name__ == "__main__":
     model.compile(loss='mse', optimizer='adamax', metrics=["mse"])
     print('Model compiled, now fitting')
 
-    history = model.fit(X_train_curr, y_train_1, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1,
+    history = model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1,
                         validation_split=0.2)
     print('Model fit, now predicting for validation set')
 
-    yPred = model.predict(X_validate_curr)
+    y_predict = model.predict(X_validate)
 
-    # acc = np.sum(yPred==y_validate)/len(y_validate)*100
-
-    v_i = np.mean(yPred[y_validate_curr == y_unique[i]]) * np.std(y_train_curr) + np.mean(y_train_curr)
-    v_j = np.mean(yPred[y_validate_curr == y_unique[j]]) * np.std(y_train_curr) + np.mean(y_train_curr)
-    v_k = np.mean(yPred[y_validate_curr == y_unique[k]]) * np.std(y_train_curr) + np.mean(y_train_curr)
-
+    v_i = np.mean(y_predict[y_validate == y_unique[i]]) * np.std(y_train) + np.mean(y_train)
     print('i - ', i, ',', y_unique[i], '-', v_i)
-    print('j - ', j, ',', y_unique[j], '-', v_j)
-    print('k - ', k, ',', y_unique[k], '-', v_k)
 
     data = {
-        'y_train': np.unique(y_train_curr),
-        'y_validate': np.unique(y_validate_curr),
+        'y_train': np.unique(y_train),
+        'y_validate': np.unique(y_validate),
         'i': i,
         'j': j,
         'k': k,
@@ -235,7 +198,6 @@ if __name__ == "__main__":
     # plt.savefig("val_loss.png")
     # plt.plot(history.history['loss'])
     # plt.savefig("train_loss.png")
-    # plt.clf()
 
     gc.collect()
 
