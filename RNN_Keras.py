0d48a9ab28562d473d8c2debd5e76d08c73ba003
Vedaant Tambi <tambi@wisc.edu>
2020-06-26 18:33:49 -0400

    Added LSTM and Dense Layer

--- RNN_Keras.py
+++ RNN_Keras.py
@@ -7,6 +7,7 @@ Created on Wed Jan  2 11:37:22 2019
 
 import numpy as np
 import matplotlib
+from tensorflow_core.python.keras.layers import LSTM
 
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
@@ -102,16 +103,20 @@ if __name__ == "__main__":
 
     # To keep only validation set entries in validation array, and to delete these from the training set
     y_unique = np.unique(y_validate)
-    validate_entries = y_unique[choose_validation_set_indices(file_name, index)]
+    fold = choose_validation_set_indices(file_name, index)
+    validate_entries = y_unique[fold]
     for val in validate_entries:
         # np.where returns the array of indices where the values in the training set coincide with the values in the
         # validation set. np.delete removes these values (from X and y)
         X_train = np.delete(X_train, np.where(y_train == val), axis=0)
         y_train = np.delete(y_train, np.where(y_train == val), axis=0)
 
+    print("Shape of X_train before:", X_train.shape)
+    # Arrange the array by time stamps
+    X_train = np.moveaxis(X_train, -1, 1)
     unflattened_shape = X_train.shape
-    print("Shape of X_train:", unflattened_shape)
-    X_train = X_train.reshape((unflattened_shape[0], -1, nDeep))
+    X_train = np.reshape(X_train, (unflattened_shape[0], unflattened_shape[1], -1))
+    print("Shape of X_train after:", X_train.shape)
 
     # Definition of size of problem
     print('Defining network')
@@ -126,34 +131,12 @@ if __name__ == "__main__":
     print('X_test shape:', X_validate.shape)
     print('y_test shape:', y_validate)
 
-    # Shape of input - 32x32x1
-    in_shape = ( nDeep)
+    # RNN Model creation
 
     model = Sequential()
-
-
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    # model.add(Convolution2D(5, (5, 5), kernel_initializer='he_normal', activation = 'relu', name='conv2'))
-    model.add(Convolution2D(12, (5, 5), kernel_initializer='he_normal', activation='relu', name='conv2'))
-    # model.add(Convolution2D(25, (5, 5), kernel_initializer='he_normal', activation = 'relu', name='conv2'))
-    # model.add(Convolution2D(25, (5, 5), kernel_initializer='he_normal', activation = 'relu', name='conv2'))
-
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Flatten())
-    model.add(Dense(180, activation='relu', kernel_initializer='he_normal', name='dense1'))
-    model.add(Dropout(0.5))
-    model.add(Dense(50, activation='relu', kernel_initializer='he_normal', name='dense2'))
-    model.add(Dropout(0.5))
-    # model.add(Dense(nb_classes, activation='softmax', kernel_initializer='he_normal', name='dense3'))
-
-    model.add(Dense(1, activation='linear', kernel_initializer='he_normal', name='dense3'))
-
-    # adamax = keras.optimizers.Adamax(lr=0.000, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
-
-    # model.optimizer.lr.set_value(0)
-
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(Dense(32, activation='linear'))
     model.compile(loss='mse', optimizer='adamax', metrics=["mse"])
     print('Model compiled, now fitting')
 
@@ -161,27 +144,12 @@ if __name__ == "__main__":
                         validation_split=0.2)
     print('Model fit, now predicting for validation set')
 
+    #  Each point in X_validate is predicted
     y_predict = model.predict(X_validate)
-
-    v_i = np.mean(y_predict[y_validate == y_unique[i]]) * np.std(y_train) + np.mean(y_train)
-    print('i - ', i, ',', y_unique[i], '-', v_i)
 
     data = {
         'y_train': np.unique(y_train),
         'y_validate': np.unique(y_validate),
-        'i': i,
-        'j': j,
-        'k': k,
-        'y_unique': y_unique,
-        'v_i': v_i,
-        'v_j': v_j,
-        'v_k': v_k,
-        'std': np.std(y_train_curr),
-        'mean': np.mean(y_train_curr),
-        'yPred': yPred,
-        'validate_entries': validate_entries,
-        'X_val': X_validate_curr,
-        'y_val': y_validate_curr
     }
     file_name = output_folder + 'regress_store_' + str(i) + '_j_' + str(j) + '_k_' + str(k) + '.pickle'
     pickle_in = open(file_name, 'wb')
